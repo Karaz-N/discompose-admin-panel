079246dbe650b7eafb3522ed1d08f7e3ee093656
UnknownEquineCoderFH <21imc10066@fh-krems.ac.at>
2024-03-19 16:11:10 +0100

    dump

---
+++ bruteforce-migrate.sh
@@ -0,0 +1,9 @@
+"""
+postgresql://postgres:fBGGagC3fEBdf5b54aCg134bC4c12fD3@viaduct.proxy.rlwy.net:43719/railway
+"""
+
+pg_dump --host ec2-46-137-91-216.eu-west-1.compute.amazonaws.com --port 5432 --user opnmyfngbknppm d50rgmkqi2ipus
+
+// fBGGagC3fEBdf5b54aCg134bC4c12fD3
+
+pg_dump --host viaduct.proxy.rlwy.net --port 43719 --user postgres railway > dump.sql
