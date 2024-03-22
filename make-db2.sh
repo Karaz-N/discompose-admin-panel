118057ec8d0f8e7f5c44356dc3ff418fe2e032a1
UnknownEquineCoderFH <21IMC10066@fh-krems.ac.at>
2024-03-22 12:30:43 +0100

    added flag

--- make-db2.sh
+++ make-db2.sh
@@ -1,5 +1,6 @@
 sudo docker build . -t database -f ./Dockerfile.db \
     && sudo docker run \
+        --privileged \
         --name database \
         -d -p 5432:5432 \
         -e POSTGRES_PASSWORD=password \
